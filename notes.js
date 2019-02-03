4dc383468eb523f964b8b3629973e2b42adef047
jgeorge <jasontgeorge1@gmail.com>
2019-02-03 11:49:48 -0800

    Add note function implemented and updated gitignore to ignore notes-data.js file

--- notes.js
+++ notes.js
@@ -1,7 +1,35 @@
 console.log('starting notes');
 
+const fs = require("fs");
+
 var addNote = (title, body) => {
-  console.log("Adding note\nTitle:", title, "\nBody:", body);
+  var notes = [];
+  var note = {
+    title, body
+  };
+  try {
+    var notesString = fs.readFileSync("notes-data.json");
+    notes = JSON.parse(notesString);
+  } catch (e) {  }
+
+  // Need to make sure the note title doesn't match an existing title
+
+  var duplicateNotes = notes.filter((note) => {
+    return note.title === title;
+  });
+
+  if (duplicateNotes.length == 0) {
+
+    notes.push(note);
+    console.log(notes);
+    fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+    console.log("note added successfully");
+
+  } else {
+    console.log('duplicate note.')
+  }
+
+
 };
 
 var removeNote = (title) => {
